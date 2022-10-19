ef978b8d6c77660b06386103cbd8f316a7a29f22
mcanteros2021 <alum.mcanteros.2021@iesalixar.org>
2022-10-19 21:17:28 +0200

    solo ejercicio we storage

--- Practica3DWES/ejercicio5/cookie.js
+++ Practica3DWES/ejercicio5/cookie.js
@@ -4,17 +4,6 @@ function crearUnaCookie(nombreCookie,datosCookie){
     console.log(cookies)
 }
 
-function checkCookie() {
-    let username = getCookie("username"); //extrae el valor función anterior
-    if (username != "") { //Si no está vacío
-   alert(("Bienvenido de nuevo, " + username)); //Se le saluda
-    } else {
-   username = prompt("Introduzca su nombre: ", "");
-   if (username != "" && username != null) {
-    setCookie("username", username, 365); //Se inicializa la variable
-   }
-    }
-   }
 function consultarCookie(){
     let galleta = document.cookie;
     console.log(galleta)
@@ -23,3 +12,4 @@ function eliminarCookie(cookies){
     document.cookie = cookies + "=; max-age=0";
     console.log(document.cookie)
 }
+
